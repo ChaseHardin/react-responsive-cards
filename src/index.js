84623eae4bed680b8c6bca7ee7ffa371f2fa8fe8
Chase Hardin <chase.hardin@scribbletechnology.com>
2020-08-16 16:48:23 -0500

    Fixlint

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Alert from 'react-bootstrap/Alert';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Alert from 'react-bootstrap/Alert'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
 
 export const ExampleComponent = ({ text }) => {
   return (
     <Container>
-      <Row style={{marginTop: '2rem'}}>
+      <Row style={{ marginTop: '2rem' }}>
         <Col>
-          <Alert variant={'info'}>Example Component: {text}</Alert>
+          <Alert variant='info'>Example Component: {text}</Alert>
         </Col>
       </Row>
     </Container>
