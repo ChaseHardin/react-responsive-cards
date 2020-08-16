642fc5f7af733d15581a2c1ac61bdbe97ffacc17
Chase Hardin <dchasehardin@gmail.com>
2020-08-16 16:27:11 -0500

    Add bootstrap to project.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
-import styles from './styles.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Alert from 'react-bootstrap/Alert';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  return (
+    <Container>
+      <Row style={{marginTop: '2rem'}}>
+        <Col>
+          <Alert variant={'info'}>Example Component: {text}</Alert>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
