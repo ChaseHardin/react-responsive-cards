3ef2669b32303bf231492c88b033bb6f91eee5d1
Chase Hardin <chase.hardin@scribbletechnology.com>
2020-08-16 18:19:07 -0500

    Add chance

--- src/show-multiple-cards.test.js
+++ src/show-multiple-cards.test.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { Cards } from './index'
+import chance from './test-utils/chance-util'
 
 test('should allow a user to view multiple cards', () => {
   const details = [
     {
-      title: 'Card 1',
-      description: 'A short description for card 1'
+      title: chance.string(),
+      description: chance.paragraph()
     },
     {
-      title: 'Card 2',
-      description: 'A short description for card 2'
+      title: chance.string(),
+      description: chance.paragraph()
     }
   ]
 
