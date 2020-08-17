ab072b5170517caa7b81f22b74cecffd98524137
Chase Hardin <chase.hardin@scribbletechnology.com>
2020-08-16 19:37:59 -0500

    Add image

--- src/card.js
+++ src/card.js
@@ -2,8 +2,20 @@ import React from 'react'
 import Card from 'react-bootstrap/Card'
 
 export default ({ detail }) => {
+  const makeAltText = `Picture for ${detail.title}`
+  const renderImage = () =>
+    detail.image && (
+      <Card.Img
+        alt={makeAltText}
+        variant='top'
+        src={detail.image}
+        style={{ width: '100%', height: '400px', objectFit: 'cover' }}
+      />
+    )
+
   return (
-    <Card style={{ width: '100%', height: '200px' }}>
+    <Card>
+      {renderImage()}
       <Card.Body>
         <Card.Title>{detail.title}</Card.Title>
         <Card.Text>{detail.description}</Card.Text>
