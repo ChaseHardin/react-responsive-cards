5cebf9b81404a92dc3fee6da910e198f9ad0c499
Chase Hardin <dchasehardin@gmail.com>
2020-08-16 19:37:59 -0500

    Add image

--- src/show-multiple-cards.test.js
+++ src/show-multiple-cards.test.js
@@ -7,18 +7,34 @@ test('should allow a user to view multiple cards', () => {
   const details = [
     {
       title: chance.string(),
-      description: chance.paragraph()
+      description: chance.paragraph(),
+      image: chance.url()
     },
+    {
+      title: chance.string(),
+      description: chance.paragraph(),
+      image: chance.url()
+    }
+  ]
+
+  const { getByText, getByAltText } = render(<Cards details={details} />)
+
+  details.forEach((detail) => {
+    getByText(detail.title)
+    getByText(detail.description)
+    getByAltText(`Picture for ${detail.title}`)
+  })
+})
+
+test('should allow a user to provide no images', () => {
+  const details = [
     {
       title: chance.string(),
       description: chance.paragraph()
     }
   ]
 
-  const { getByText } = render(<Cards details={details} />)
+  const { queryByAltText } = render(<Cards details={details} />)
 
-  details.forEach((detail) => {
-    getByText(detail.title)
-    getByText(detail.description)
-  })
+  expect(queryByAltText(`Picture for ${details[0].title}`)).toBeNull()
 })
