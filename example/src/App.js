ae7b2122031d099c6f305e974ab8a2fff756db0d
Chase Hard <dchasehardin@gmail.com>
2020-11-07 12:19:15 -0600

    Make material-ui a peerDependency

--- example/src/App.js
+++ example/src/App.js
@@ -1,25 +1,17 @@
 import React from 'react'
-
 import { Cards } from 'react-responsive-cards'
-import 'react-responsive-cards/dist/index.css'
 import Chance from 'chance'
 import CardActions from '@material-ui/core/CardActions';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import IconButton from '@material-ui/core/IconButton';
-import ShareIcon from '@material-ui/icons/Share'
 
 const chance = new Chance()
 
 const renderFooter = title => {
-  const handleClick = () => alert(title);
-
   return (
     <CardActions>
       <IconButton aria-label="add to favorites">
-        <FavoriteIcon onClick={handleClick} />
-      </IconButton>
-      <IconButton aria-label="add to favorites">
-        <ShareIcon onClick={handleClick} />
+        <FavoriteIcon />
       </IconButton>
     </CardActions>
   )
